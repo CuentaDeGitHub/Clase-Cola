635f407c60ddbf998c5076977e72fec1ecf7bc8c
CuentaDeGitHub <60906225+CuentaDeGitHub@users.noreply.github.com>
2020-11-05 10:24:14 -0600

    Add files via upload

---
+++ ClaseCola/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ClaseCola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQueue = new System.Windows.Forms.Button();
+            this.btnDequeue = new System.Windows.Forms.Button();
+            this.btnPeek = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnContains = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.lblCola = new System.Windows.Forms.Label();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQueue
+            // 
+            this.btnQueue.Location = new System.Drawing.Point(143, 12);
+            this.btnQueue.Name = "btnQueue";
+            this.btnQueue.Size = new System.Drawing.Size(75, 23);
+            this.btnQueue.TabIndex = 0;
+            this.btnQueue.Text = "Enqueue";
+            this.btnQueue.UseVisualStyleBackColor = true;
+            this.btnQueue.Click += new System.EventHandler(this.btnQueue_Click);
+            // 
+            // btnDequeue
+            // 
+            this.btnDequeue.Location = new System.Drawing.Point(143, 41);
+            this.btnDequeue.Name = "btnDequeue";
+            this.btnDequeue.Size = new System.Drawing.Size(75, 23);
+            this.btnDequeue.TabIndex = 1;
+            this.btnDequeue.Text = "Dequeue";
+            this.btnDequeue.UseVisualStyleBackColor = true;
+            this.btnDequeue.Click += new System.EventHandler(this.btnDequeue_Click);
+            // 
+            // btnPeek
+            // 
+            this.btnPeek.Location = new System.Drawing.Point(143, 70);
+            this.btnPeek.Name = "btnPeek";
+            this.btnPeek.Size = new System.Drawing.Size(75, 23);
+            this.btnPeek.TabIndex = 2;
+            this.btnPeek.Text = "Peek";
+            this.btnPeek.UseVisualStyleBackColor = true;
+            this.btnPeek.Click += new System.EventHandler(this.btnPeek_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(143, 99);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnContains
+            // 
+            this.btnContains.Location = new System.Drawing.Point(143, 128);
+            this.btnContains.Name = "btnContains";
+            this.btnContains.Size = new System.Drawing.Size(75, 23);
+            this.btnContains.TabIndex = 5;
+            this.btnContains.Text = "Contains";
+            this.btnContains.UseVisualStyleBackColor = true;
+            this.btnContains.Click += new System.EventHandler(this.btnContains_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dato";
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(69, 14);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(72, 20);
+            this.txtDato.TabIndex = 7;
+            // 
+            // lblCola
+            // 
+            this.lblCola.AutoSize = true;
+            this.lblCola.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCola.Location = new System.Drawing.Point(271, 88);
+            this.lblCola.Name = "lblCola";
+            this.lblCola.Size = new System.Drawing.Size(41, 20);
+            this.lblCola.TabIndex = 8;
+            this.lblCola.Text = "Cola";
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(143, 157);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 9;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 186);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.lblCola);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnContains);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPeek);
+            this.Controls.Add(this.btnDequeue);
+            this.Controls.Add(this.btnQueue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQueue;
+        private System.Windows.Forms.Button btnDequeue;
+        private System.Windows.Forms.Button btnPeek;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnContains;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Label lblCola;
+        private System.Windows.Forms.Button btnCount;
+    }
+}
+
---
+++ ClaseCola/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClaseCola
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> miCola = new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQueue_Click(object sender, EventArgs e)
+        {
+            if(txtDato.Text == "")
+            {
+                return;
+            }
+            miCola.Enqueue(txtDato.Text);
+            string cola = "";
+            foreach (string s in miCola)
+            {
+                cola += s + " ";
+            }
+            lblCola.Text = cola;
+            txtDato.Clear();
+
+        }
+
+        private void btnDequeue_Click(object sender, EventArgs e)
+        {
+            if (miCola.Count > 0)
+            {
+                string datoeliminado = miCola.Dequeue();
+                MessageBox.Show("Dato desencolado : " + datoeliminado);
+                string cola = "";
+                foreach (string s in miCola)
+                {
+                    cola += s + " ";
+                }
+                lblCola.Text = cola;
+                txtDato.Clear();
+                return;
+            }
+            MessageBox.Show("La cola esta vacia");
+          
+        }
+
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            if (miCola.Count > 0)
+            {
+                string datoeliminado = miCola.Peek();
+                MessageBox.Show("Dato en el frente : " + datoeliminado);
+                return;
+            }
+            MessageBox.Show("La cola esta vacia");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            miCola.Clear();
+            lblCola.Text = "";
+        }
+
+        private void btnContains_Click(object sender, EventArgs e)
+        {
+            if (txtDato.Text != "")
+            {
+                string dato = txtDato.Text;
+                bool encontrado = miCola.Contains(dato);
+                if(encontrado == true)
+                {
+                    MessageBox.Show("El dato " + dato + " fue encontrado");
+                }
+                else
+                {
+                    MessageBox.Show("El dato " + dato + " no fue encontrado");
+                }
+                return;
+            }
+
+            
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Numero de elementos en la cola : " + miCola.Count);
+        }
+    }
+}
